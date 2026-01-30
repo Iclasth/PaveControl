17f653c21b9d6648a505b397cb953723f7e500e9
iclasTH <iclasinprogramacao@gmail.com>
2026-01-29 21:47:30 -0300

    Adicionar arquivos de projeto.

---
+++ PaveControl/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaveControl.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do pavê é obrigatório")]
+        [Display(Name = "Nome do sabor")]
+        public string Nome { get; set; } = string.Empty; 
+
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        [Display(Name = "Preço da venda")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Quantidade em Estoque")]
+        public int Estoque { get; set; }
+
+        public bool Ativo { get; set; } = true;
+    }
+}
